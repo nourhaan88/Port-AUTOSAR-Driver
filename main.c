1a660feebb2c755a78e8a80158d22720a603c447
nourhaan88 <104329861+nourhaan88@users.noreply.github.com>
2022-08-24 17:48:23 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+ *
+ * Module: Main
+ *
+ * File Name: Main.c
+ *
+ * Description: Source file for Main Module.
+ *
+ * Author: Nourhan Ehab
+ ******************************************************************************/
+
+
+#include "Os.h"
+
+int main(void)
+{
+    /* Start the Os */
+    Os_start();
+}
